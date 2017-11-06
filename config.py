f06d856995b46089d017699fcebe5a73d37e2ad4
NikeMrr <33435567+NikeMrr@users.noreply.github.com>
2017-11-06 23:51:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Basic Config (For all bots)
-userToken = ['3956'] # User Token here, multiple accounts may work. example 'token','token'
-DiscordChannel = '362927686122864640' # Channel ID,
+userToken = ['-2nfMFuUcG-7WW-nX1_EbHWDNmu_40r6Iard3rNZd2VehKA9B34TDS27PiILWJ6yVqc-'] # User Token here, multiple accounts may work. example 'token','token'
+DiscordChannel = '362931485444931596' # Channel ID,
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Emanuel\\Pictures\\prntime.jpg\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
