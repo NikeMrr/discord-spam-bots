bb4ad23ecb710d0016bf2221a15324bb19cb582c
NikeMrr <33435567+NikeMrr@users.noreply.github.com>
2017-11-06 23:44:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ userToken = ['3956'] # User Token here, multiple accounts may work. example 'tok
 DiscordChannel = '362927686122864640' # Channel ID,
 
 #Picture spam
-DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
+DirPictures = 'C:\\Users\\Public\\Emanuel\\Pictures\\prntime.jpg\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
