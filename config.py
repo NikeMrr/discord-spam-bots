a8c41c4536008d4100f360fa798106761476db5e
NikeMrr <33435567+NikeMrr@users.noreply.github.com>
2017-11-06 23:41:45 +0100

    Prueba de configuración
    Esta es la primera para ver si los token son estos

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Basic Config (For all bots)
-userToken = [''] # User Token here, multiple accounts may work. example 'token','token'
-DiscordChannel = '' # Channel ID,
+userToken = ['3956'] # User Token here, multiple accounts may work. example 'token','token'
+DiscordChannel = '362927686122864640' # Channel ID,
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
